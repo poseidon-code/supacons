15ea838ed87a2dbe73d21c0576f49c8cc17fbd0c
poseidon-code <pritamhalder05062000@gmail.com>
2022-05-30 12:00:00 +0530

    remove: copy, download svg icons

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -122,17 +122,6 @@ export const MagnifyingGlass = () => (
     </svg>
 );
 
-export const Copy = () => (
-    <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-        <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth={2}
-            d='M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z'
-        />
-    </svg>
-);
-
 export const NotFound = () => (
     <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
         <path
@@ -150,17 +139,6 @@ export const More = () => (
             fillRule='evenodd'
             d='M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z'
             clipRule='evenodd'
-        />
-    </svg>
-);
-
-export const Downlaod = () => (
-    <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-        <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth={2}
-            d='M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4'
         />
     </svg>
 );
