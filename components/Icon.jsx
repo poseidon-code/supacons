e495cefe056f60ce3daa271d8dc87cac02aebbb1
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2022-10-24 21:33:46 +0530

    refactor: icons showcase for 6.2.0

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -1,37 +1,41 @@
 import styles from '../styles/Icon.module.css';
 
-const Icon = ({ name, type, clipboard: { copier } }) => {
-    const copyIconTag = () => {
-        copier(`<i class='fa-${type} fa-${name}></i>`);
+const Icon = ({ name, type, subtypes, clipboard: { copier } }) => {
+    const copyIconTag = (type, subtype, name) => {
+        copier(`<i class='${type === 'sharp' ? 'fa-sharp ' : ''}fa-${subtype} fa-${name}></i>`);
     };
 
-    return (
-        <div
-            className={styles.icon}
-            id='copy'
-            onClick={copyIconTag}
-            title={`<i className='fa-${type} fa-${name}'></i>`}>
-            <span className='badge'>
-                {type === 'brands'
-                    ? 'B'
-                    : type === 'duotone'
-                    ? 'D'
-                    : type === 'solid'
-                    ? 'S'
-                    : type === 'regular'
-                    ? 'R'
-                    : type === 'light'
-                    ? 'L'
-                    : type === 'thin'
-                    ? 'T'
-                    : null}
-            </span>
-            <div className={styles.font_icon}>
-                <i className={`fa-${type} fa-${name}`}></i>
+    return subtypes.map((subtype, i) => {
+        return (
+            <div
+                className={styles.icon}
+                key={`${type}-${subtype}-${name}-${i}`}
+                id='copy'
+                onClick={() => copyIconTag(type, subtype, name)}
+                title={`<i className='${type === 'sharp' ? 'fa-sharp ' : ''}fa-${subtype} fa-${name}'></i>`}>
+                <span className='badge'>
+                    <sup>{type === 'brands' ? 'B' : type === 'classic' ? 'C' : type === 'sharp' ? 'S' : null}</sup>
+                    {subtype === 'brands'
+                        ? 'B'
+                        : subtype === 'duotone'
+                        ? 'D'
+                        : subtype === 'solid'
+                        ? 'S'
+                        : subtype === 'regular'
+                        ? 'R'
+                        : subtype === 'light'
+                        ? 'L'
+                        : subtype === 'thin'
+                        ? 'T'
+                        : null}
+                </span>
+                <div className={styles.font_icon}>
+                    <i className={`${type === 'sharp' ? 'fa-sharp ' : ''}fa-${subtype} fa-${name}`}></i>
+                </div>
+                <span className={styles.name}>{name}</span>
             </div>
-            <span className={styles.name}>{name}</span>
-        </div>
-    );
+        );
+    });
 };
 
 export default Icon;
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,24 +4,69 @@ import styles from '../styles/index.module.css';
 import Icon from '../components/Icon';
 import { More } from '../components/Icons';
 
-const Home = ({ icons, total_icons, clipboard }) => {
+const Home = ({ icons, clipboard }) => {
     const [count, setCount] = useState(400);
     const [load, setLoad] = useState(true);
+    const [allIcons, setAllIcons] = useState([]);
+
+    const typeClassic = ['solid', 'regular', 'light', 'thin', 'duotone'];
+    const typeSharp = ['solid'];
+    const typeBrand = ['brands'];
 
     const handleLoadMore = () => {
         setCount(p => p + 400);
     };
 
     useEffect(() => {
-        if (count > total_icons) setLoad(false);
+        let flattenIcons = [];
+        for (let name in icons.brands) {
+            const icon = {
+                name: name,
+                type: 'brands',
+                subtypes: typeBrand,
+            };
+            flattenIcons.push(icon);
+        }
+        for (let name in icons.generic) {
+            const icon = {
+                name: name,
+                type: 'classic',
+                subtypes: typeClassic,
+            };
+            flattenIcons.push(icon);
+        }
+        for (let name in icons.generic) {
+            const icon = {
+                name: name,
+                type: 'sharp',
+                subtypes: typeSharp,
+            };
+            flattenIcons.push(icon);
+        }
+
+        flattenIcons.sort((a, b) => {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+        });
+
+        setAllIcons(flattenIcons);
+
+        if (count > flattenIcons.length) setLoad(false);
     }, [count]);
 
     return (
         <section className='container' style={{ margin: '3rem 0' }}>
             <div className='wrapper'>
                 <div className={styles.icons}>
-                    {icons.slice(0, count).map((icon, i) => (
-                        <Icon key={i} clipboard={clipboard} name={icon.name} type={icon.type} />
+                    {allIcons.slice(0, count).map((icon, i) => (
+                        <Icon
+                            key={i}
+                            clipboard={clipboard}
+                            name={icon.name}
+                            type={icon.type}
+                            subtypes={icon.subtypes}
+                        />
                     ))}
                 </div>
                 {load && (
@@ -43,12 +88,10 @@ export default Home;
 export const getStaticProps = async () => {
     const getIcons = (await import('../getIcons')).default;
     const icons = await getIcons();
-    const total_icons = icons.length;
 
     return {
         props: {
             icons,
-            total_icons,
         },
     };
 };
