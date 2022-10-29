fe6a6fffdacf6f6072cf6964d4ffb752eee35bc2
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2022-10-29 20:53:38 +0530

    refactor: loding screen

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,16 +1,18 @@
 import styles from '../styles/Loading.module.css';
 
-const Loading = () => {
-    return (
-        <section className={styles.loading}>
-            <div className={styles.loader}>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </section>
-    );
-};
+const Loading = () => (
+    <section className={styles.loading}>
+        <Spinner />
+    </section>
+);
 
 export default Loading;
+
+export const Spinner = () => (
+    <div className={styles.loader}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
