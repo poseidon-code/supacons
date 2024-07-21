279d9888219b00a850ec4186b299ea48bcfa915e
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2024-07-21 09:41:45 +0530

    add: supacons 6.6.0

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
-import { generic, brands } from './icons.js';
+import { generic, brands } from "./icons.js";
 
-const classicIcons = document.getElementById('classic');
-const sharpIcons = document.getElementById('sharp');
-const brandIcons = document.getElementById('brands');
+const classicIcons = document.getElementById("classic");
+const sharpIcons = document.getElementById("sharp");
+const brandIcons = document.getElementById("brands");
 
 for (let name in generic) {
     const newRow = classicIcons.insertRow(classicIcons.rows.length);
@@ -23,9 +23,9 @@ for (let name in generic) {
     newRow.insertCell(1).innerText = generic[name];
     newRow.insertCell(2).innerHTML = `<i class="fa-sharp fa-solid fa-${name}"></i>`;
     newRow.insertCell(3).innerHTML = `<i class="fa-sharp fa-regular fa-${name}"></i>`;
-    newRow.insertCell(4).innerHTML = `<i class="fa-sharp fa-light fa-${name}"></i>`
-    newRow.insertCell(5).innerHTML = `<i class="fa-sharp fa-thin fa-${name}"></i>`
-    // newRow.insertCell(6).innerHTML = `<i class="fa-sharp fa-duotone fa-${name}"></i>`
+    newRow.insertCell(4).innerHTML = `<i class="fa-sharp fa-light fa-${name}"></i>`;
+    newRow.insertCell(5).innerHTML = `<i class="fa-sharp fa-thin fa-${name}"></i>`;
+    newRow.insertCell(6).innerHTML = `<i class="fa-sharp fa-duotone fa-${name}"></i>`;
 }
 
 for (let name in brands) {
