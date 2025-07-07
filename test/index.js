30bce424ebe9d239f3d8f8dd7fbf0441e0573129
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2025-07-07 21:25:54 +0530

    update: supacons 6.7.2 icon tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 import { generic, brands } from "./icons.js";
 
 const classicIcons = document.getElementById("classic");
+const duotoneIcons = document.getElementById("duotone");
+const sharpDuotoneIcons = document.getElementById("sharp_duotone");
 const sharpIcons = document.getElementById("sharp");
 const brandIcons = document.getElementById("brands");
 
@@ -13,7 +15,28 @@ for (let name in generic) {
     newRow.insertCell(3).innerHTML = `<i class="fa-regular fa-${name}"></i>`;
     newRow.insertCell(4).innerHTML = `<i class="fa-light fa-${name}"></i>`;
     newRow.insertCell(5).innerHTML = `<i class="fa-thin fa-${name}"></i>`;
-    newRow.insertCell(6).innerHTML = `<i class="fa-duotone fa-${name}"></i>`;
+}
+
+for (let name in generic) {
+    const newRow = duotoneIcons.insertRow(duotoneIcons.rows.length);
+
+    newRow.insertCell(0).innerText = name;
+    newRow.insertCell(1).innerText = generic[name];
+    newRow.insertCell(2).innerHTML = `<i class="fa-duotone fa-solid fa-${name}"></i>`;
+    newRow.insertCell(3).innerHTML = `<i class="fa-duotone fa-regular fa-${name}"></i>`;
+    newRow.insertCell(4).innerHTML = `<i class="fa-duotone fa-light fa-${name}"></i>`;
+    newRow.insertCell(5).innerHTML = `<i class="fa-duotone fa-thin fa-${name}"></i>`;
+}
+
+for (let name in generic) {
+    const newRow = sharpDuotoneIcons.insertRow(sharpDuotoneIcons.rows.length);
+
+    newRow.insertCell(0).innerText = name;
+    newRow.insertCell(1).innerText = generic[name];
+    newRow.insertCell(2).innerHTML = `<i class="fa-sharp-duotone fa-solid fa-${name}"></i>`;
+    newRow.insertCell(3).innerHTML = `<i class="fa-sharp-duotone fa-regular fa-${name}"></i>`;
+    newRow.insertCell(4).innerHTML = `<i class="fa-sharp-duotone fa-light fa-${name}"></i>`;
+    newRow.insertCell(5).innerHTML = `<i class="fa-sharp-duotone fa-thin fa-${name}"></i>`;
 }
 
 for (let name in generic) {
@@ -25,7 +48,6 @@ for (let name in generic) {
     newRow.insertCell(3).innerHTML = `<i class="fa-sharp fa-regular fa-${name}"></i>`;
     newRow.insertCell(4).innerHTML = `<i class="fa-sharp fa-light fa-${name}"></i>`;
     newRow.insertCell(5).innerHTML = `<i class="fa-sharp fa-thin fa-${name}"></i>`;
-    newRow.insertCell(6).innerHTML = `<i class="fa-sharp fa-duotone fa-${name}"></i>`;
 }
 
 for (let name in brands) {
